d1b4435641dd3c41b950b93cf918f30707d04aad
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-06-04 10:14:20 +0200

    changed placeholder to auswählen

--- webapp/src/components/Eingabealphabet/DropDownSelect.tsx
+++ webapp/src/components/Eingabealphabet/DropDownSelect.tsx
@@ -37,7 +37,7 @@ export default function DropDownSelect() {
     return (
         <div className={"bg-white w-screen sm:w-3/4 lg:w-2/4 xl:w-1/4 grid grid-cols-2 gap-2 items-center"}>
             <p className={"p-3"}>Eingabealphabet ∑ =</p>
-            <Select placeholder={<p>wählen Sie Ihr Eingabealphabet...</p>}
+            <Select placeholder={<p>auswählen</p>}
                     blurInputOnSelect={false}
                     className={"text-black p-3 text-lg"}
                     onChange={handleChange}
