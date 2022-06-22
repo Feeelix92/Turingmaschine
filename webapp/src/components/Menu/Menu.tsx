4090b1bb900fd72ef7c69ad88274e7d2ea82c957
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-06-22 16:57:12 +0200

    menu padding fix

--- webapp/src/components/Menu/Menu.tsx
+++ webapp/src/components/Menu/Menu.tsx
@@ -4,7 +4,7 @@ import Sidebar from "../Sidebar/Sidebar";
 
 function Menu() {   
     return (
-        <div className={"menu w-screen pl-10 pr-10"}>
+        <div className={"menu w-screen sm:pl-10 sm:pr-10"}>
             <Sidebar />
             <h1 className="text-white text-xl p-3">
                 Turingmaschinen-Simulator
