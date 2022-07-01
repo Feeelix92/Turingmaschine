2132381f3f8e3d823fd30bdd5633f157b8b507fd
sarah2910 <sarah.wassmuth@iem.thm.de>
2022-07-01 13:06:14 +0200

    Bottomnav als Komponente für mobile Ansicht

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,6 +4,8 @@ import Table from "./components/Zustandsüberführungsfunktion/Table";
 import Menu from "./components/Menu/Menu";
 import Control from "./components/Control/Control";
 import ConditionsList from "./components/Zustaende/List";
+import Bottomnav from "./components/Bottomnav/Bottomnav";
+
 
 function App() {             
         
@@ -14,10 +16,20 @@ function App() {
                 <Control />
                 <Band/>
             </header>
+
+
             <div className={"App-body"}>
-                <ConditionsList/>
-                <Table/>
+
+                <div className={" invisible md:visible"}>
+                    <ConditionsList/>
+                    <Table/>
+                </div>
+
             </div>
+
+            <div className={"md:invisible fixed bottom-0"}>
+                    <Bottomnav/>
+                </div>
         </div>
     );
         
---
+++ webapp/src/components/Bottomnav/Bottomnav.tsx
@@ -0,0 +1,24 @@
+
+
+function Bottomnav() {
+    return (
+        <div className={"bottomnav w-screen mt-16 p-5 flex justify-around"}>
+            <button className={""}>
+                Spezifikationen
+            </button>
+
+            <button className={""}>
+                Funktion
+            </button>
+
+            
+            <button className={""}>
+                Code
+            </button>
+        </div>
+
+
+    );
+}
+
+export default Bottomnav;
