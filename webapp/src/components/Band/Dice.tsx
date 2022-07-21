31143302dcaf81d4a278b1ff47cc28ad1d370a67
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-07-21 09:22:33 +0200

    cleanup dice

--- webapp/src/components/Band/Dice.tsx
+++ webapp/src/components/Band/Dice.tsx
@@ -1,26 +1,30 @@
 import React, {useState, useEffect} from "react";
-import {bandAddField} from "../../redux/bandStore";
+import {useSelector} from "react-redux";
+import {RootState} from "../../redux/store";
 
 export default function Dice() {
-    const faces = 6;
-    const [diceFace, setDiceFace] = useState(6);
-    const [btnDisabled, setBtnDisabled] = useState(false);
-    const rollDice = (key: number) => {
-         setDiceFace(2);
+    const faces = {value: "1", label:"q1"};
+    const [diceFace, setDiceFace] = useState(1);
+    const rollDice = () => {
+         setDiceFace(Math.round(Math.random() * (5 - 1 + 1) + 1));
     }
+    const currentZustand = useSelector(
+        (state: RootState) => state.general.activeState
+    );
+    console.log(currentZustand.value);
 
 
     return (
-            <div onClick={() => setDiceFace(Math.round(Math.random() * (5 - 1 + 1) + 1))} className={`dice face${diceFace}`}>
-                <div className={`dot-container justify-items-center items-center`}>
-                    <div className="dot dot1"/>
-                    <div className="dot dot2"/>
-                    <div className="dot dot3"/>
-                    <div className="dot dot4"/>
-                    <div className="dot dot5"/>
-                    <div className="dot dot6"/>
-                    <div className="dot dot7"/>
-                </div>
+        <div onClick={() => rollDice()} className={`dice face face${diceFace}`}>
+            <div className={`dot-container justify-items-center items-center`}>
+                <div className="dot dot1"/>
+                <div className="dot dot2"/>
+                <div className="dot dot3"/>
+                <div className="dot dot4"/>
+                <div className="dot dot5"/>
+                <div className="dot dot6"/>
+                <div className="dot dot7"/>
             </div>
+        </div>
     );
 }
