5624f69c80ea88896e94f1af2e041beddd499e73
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-07-21 10:13:45 +0200

    dice showing Zustand

--- webapp/src/components/Band/Dice.tsx
+++ webapp/src/components/Band/Dice.tsx
@@ -3,19 +3,16 @@ import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
 
 export default function Dice() {
-    const faces = {value: "1", label:"q1"};
-    const [diceFace, setDiceFace] = useState(1);
-    const rollDice = () => {
-         setDiceFace(Math.round(Math.random() * (5 - 1 + 1) + 1));
-    }
     const currentZustand = useSelector(
         (state: RootState) => state.general.activeState
     );
-    console.log(currentZustand.value);
-
+    /*const [diceFace, setDiceFace] = useState(currentZustand.value.slice(1,2));
+    const rollDice = () => {
+        setDiceFace(currentZustand.value.slice(1,2));
+    }*/
 
     return (
-        <div onClick={() => rollDice()} className={`dice face face${diceFace}`}>
+        <div className={`dice face face${currentZustand.value.slice(1,2)}`}>
             <div className={`dot-container justify-items-center items-center`}>
                 <div className="dot dot1"/>
                 <div className="dot dot2"/>
