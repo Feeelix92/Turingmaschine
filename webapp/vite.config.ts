9a45198dfd3b12ebcb7e9efaa38fea434d2b850a
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-09-09 18:24:25 +0200

    name for dist folder

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: resolve(__dirname, '../webapp/alpha-build'),
+    emptyOutDir: true,
+  },
   plugins: [react()]
 })
