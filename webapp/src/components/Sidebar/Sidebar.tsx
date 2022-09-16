5dddab1b35396a734cf5ab059904332812b8409c
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-09-16 08:59:49 +0200

    Sidebar cleanup

--- webapp/src/components/Sidebar/Sidebar.tsx
+++ webapp/src/components/Sidebar/Sidebar.tsx
@@ -1,121 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
-import { useDispatch, useSelector } from "react-redux";
-import {
-    changeMespumaMode,
-    alphabetChangeCurrentMespuma,
-    activateToiletPaperMode, activateNormalMode,
-} from "../../redux/generalStore";
+import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
-import { bandResetAll } from "../../redux/bandStore";
-import { EingabeAlphabet } from "../../interfaces/CommonInterfaces";
-import { cartesianProduct } from "../../interfaces/CommonFunctions";
 import AceJsonEditor from "../codeEditor/AceJsonEditor";
 import { Link, useLocation } from "react-router-dom";
 import { FaLaptopCode } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
 import { FlagIcon } from "react-flag-kit";
-import React from "react";
 
 function Sidebar() {
-  const dispatch = useDispatch();
-
   const mode = useSelector((state: RootState) => state.general.mode);
-  // const currentAlphabet = useSelector(
-  //   (state: RootState) => state.general.currentAlphabet
-  // );
-  // const anzahlSpuren = useSelector(
-  //   (state: RootState) => state.general.anzahlSpuren
-  // );
 
-  // const changeTpMode = () => {
-  //     dispatch(bandResetAll());
-  //     dispatch(activateToiletPaperMode());
-  //     dispatch(bandResetAll());
-  // };
-  //
-  //   const changeNormalMode = () => {
-  //       dispatch(bandResetAll());
-  //       dispatch(activateNormalMode());
-  //       dispatch(bandResetAll());
-  // };
   const [showModal, setShowModal] = useState(false);
 
-  function toggleModal() {
+    function toggleModal() {
     setShowModal(!showModal);
   }
 
+  // Page reload
   function reloadPage() {
-    window.location.reload(false);
+    window.location.reload();
   }
 
   // Current Router Location:
   const location = useLocation(); // Current Pathname = location.pathname
 
-  // const changeMSMMode = (mespuma: boolean) => {
-  //   dispatch(bandResetAll());
-  //   dispatch(changeMespumaMode(mespuma));
-  //   dispatch(bandResetAll());
-  //
-  //   // if (mespuma) {
-  //   //   let literalArr: string[] = [];
-  //   //
-  //   //   let tempAlphabet = Object.assign(
-  //   //     [],
-  //   //     currentAlphabet.alphabet
-  //   //   ) as EingabeAlphabet[];
-  //   //   tempAlphabet.push({ value: "B", label: "", warningMode: false });
-  //   //
-  //   //   tempAlphabet.forEach((literal) => {
-  //   //     literalArr.push(literal.value);
-  //   //   });
-  //   //
-  //   //   let combinationArr: string[][] = [];
-  //   //
-  //   //   for (let i = 0; i < anzahlSpuren; i++) {
-  //   //     combinationArr.push(literalArr);
-  //   //   }
-  //   //
-  //   //   let cartesianArr = cartesianProduct(combinationArr);
-  //   //
-  //   //   let finalBandAlphabet: string[] = [];
-  //   //
-  //   //   cartesianArr.forEach((element: any[]) => {
-  //   //     let el = "(" + element.join() + ")";
-  //   //     finalBandAlphabet.push(el);
-  //   //   });
-  //   //
-  //   //   dispatch(
-  //   //     alphabetChangeCurrentMespuma({
-  //   //       cartesian: finalBandAlphabet,
-  //   //       alphabet: currentAlphabet,
-  //   //     })
-  //   //   );
-  //   // };
-  // }
-
-  // useEffect(() => {
-  //   window.performance;
-  //   return () => {
-  //     if (location.pathname === "/mehrspuren") {
-  //       changeMSMMode(true);
-  //     } else if (location.pathname === "/papier") {
-  //       changeTpMode();
-  //     } else {
-  //       changeMSMMode(false);
-  //     }
-  //   };
-  // }, []);
-
-  const { i18n, t } = useTranslation(["general"]);
-
-  /*useEffect(() => {
-    if (localStorage.getItem("i18nextLng")?.length > 2) {
-      i18next.changeLanguage("de")
-    }
-  }, []);*/
-
   ///Wechselt die Sprache///
+  const { i18n, t } = useTranslation(["general"]);
   const handleLanguageChange = (language: { target: { value: string } }) => {
     i18n.changeLanguage(language.target.value);
   };
@@ -132,6 +43,7 @@ function Sidebar() {
       className='menu-options'
     >
       <div className={"mt-0"}>
+        {/*Link zum default Modus*/}
         <Link className="no-underline text-white text-lg" to="/">
             {t("sidebar.homePage")}
         </Link>
@@ -147,13 +59,13 @@ function Sidebar() {
 
       <div className="hidden md:inline-flex">
         <div className="rounded-md shadow-sm text-center inline-flex">
+          {/*Link zum default Modus*/}
           <Link to="/"
             className={`no-underline py-2 px-4 text-sm font-medium rounded-l-lg text-white hover:bg-gray-900 cursor-pointer border border-thm-primary2
                 ${location.pathname === "/" ? "bg-thm-primary" : "bg-gray-700 "}`}
-            // onClick={() =>changeMSMMode(false)}
-        >{t("sidebar.normalMode")}
+          >{t("sidebar.normalMode")}
           </Link>
-
+          {/*Link zum Toilettenpapiermodus*/}
           <Link to="/papier"
             className={`no-underline py-2 px-4 text-sm font-medium text-white hover:bg-gray-900 cursor-pointer border border-thm-primary2
                 ${
@@ -161,10 +73,9 @@ function Sidebar() {
                     ? "bg-thm-primary"
                     : "bg-gray-700 "
                 }`}
-            // onClick={() => changeTpMode()}
-        >{t("sidebar.toiletPaperMode")}
+          >{t("sidebar.toiletPaperMode")}
           </Link>
-
+          {/*Link zum Mehrspurenmodus*/}
           <Link to="/mehrspuren"
             className={`no-underline py-2 px-4 text-sm rounded-r-md font-medium text-white hover:bg-gray-900 cursor-pointer border border-thm-primary2
                 ${
@@ -172,8 +83,7 @@ function Sidebar() {
                     ? "bg-thm-primary"
                     : "bg-gray-700 "
                 }`}
-            // onClick={() => changeMSMMode(true)}
-        >{t("sidebar.multiTrackMachine")}
+          >{t("sidebar.multiTrackMachine")}
           </Link>
         </div>
       </div>
