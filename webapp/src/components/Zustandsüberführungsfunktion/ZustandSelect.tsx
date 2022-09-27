2a8ce28bf4415591b3e819f579335fcdaa3a0a8e
Chiara <chiara.knipprath@iem.thm.de>
2022-09-27 17:09:13 +0200

    push for felix

--- webapp/src/components/Zustandsüberführungsfunktion/ZustandSelect.tsx
+++ webapp/src/components/Zustandsüberführungsfunktion/ZustandSelect.tsx
@@ -1,35 +1,52 @@
+import { init } from "i18next";
+import { useEffect } from "react";
 import { CgAddR } from "react-icons/cg";
 import { useDispatch, useSelector } from "react-redux";
-import Select, {ActionMeta, OnChangeValue} from "react-select";
-import {Direction, Zustand, ZustandSelectProps} from "../../interfaces/CommonInterfaces";
+import Select, { ActionMeta, OnChangeValue } from "react-select";
+import {
+  Direction,
+  Zustand,
+  ZustandSelectProps,
+} from "../../interfaces/CommonInterfaces";
 import { alphabetPushToZustand } from "../../redux/generalStore";
 import { RootState } from "../../redux/store";
 
 export default function ZustandSelect(props: ZustandSelectProps) {
   const dispatch = useDispatch();
+
+  const initZustandsmenge = useSelector(
+    (state: RootState) => state.general.zustandsmenge
+  );
+  let zustandsMenge = initZustandsmenge;
+  useEffect(() => {
+    console.log("hello");
+    zustandsMenge = initZustandsmenge;
+  }, [initZustandsmenge]);
   /**
    * function handleChange checks if the selected option has changed
    * @param newValue
    * @param _actionMeta
    */
   function handleChange(
-      newValue: OnChangeValue<Zustand, false>,
-      _actionMeta: ActionMeta<Direction | Zustand>) {
+    newValue: OnChangeValue<Zustand, false>,
+    _actionMeta: ActionMeta<Direction | Zustand>
+  ) {
     let state = newValue as Zustand;
 
     if (mode !== "toiletpaper") {
       if (newValue?.label === "+") {
-        state = new Zustand(
-          `q${props.states.length}`,
-          `q${props.states.length}`,
-          false,
-          false,
-          false
-        );
         dispatch(alphabetPushToZustand());
+        setTimeout(() => {
+          console.log("zustandsMenge", initZustandsmenge);
+          state = zustandsMenge[zustandsMenge.length - 1];
+          console.log(zustandsMenge);
+          props.updateValue(state);
+        }, 1000);
+      } else {
+        console.log(zustandsMenge);
+        props.updateValue(state);
       }
     }
-    props.updateValue(state);
   }
 
   const mode = useSelector((state: RootState) => state.general.mode);
