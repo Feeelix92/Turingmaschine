cfd19b46419c1ea111ee617d79680c957c1b82e0
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-09-28 15:44:13 +0200

    added examples for even unary or even binary

--- webapp/src/examples/Examples.tsx
+++ webapp/src/examples/Examples.tsx
@@ -1,12 +1,14 @@
 import { CodeExample } from "../interfaces/CommonInterfaces";
 import addUn from "./addUn.json";
 import incUn from "./incUn.json";
-import accEvenUn from "./accEvenUn.json";
 import incBin from "./incBin.json";
+import accEvenUn from "./accEvenUn.json";
+import accEvenBin from "./accEvenBin.json";
 
 export const examples: CodeExample[] = [
   { label: "addUn", value: JSON.stringify(addUn), type: "normal" },
   { label: "incUn", value: JSON.stringify(incUn), type: "normal" },
   { label: "incBin", value: JSON.stringify(incBin), type: "normal" },
-  { label: "accEvenUn", value: JSON.stringify(accEvenUn), type: "normal" },
+  // { label: "accEvenUn", value: JSON.stringify(accEvenUn), type: "normal" },
+  // { label: "accEvenBin", value: JSON.stringify(accEvenBin), type: "normal" },
 ];
